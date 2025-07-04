8e85303937d1d9298f5928faadc6c53d9f25c2d3
Diya Tripathi <tripathidiya673@gmail.com>
2025-07-04 13:39:46 +0530

    route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,14 +1,28 @@
+// import { serve } from "inngest/next";
+// import {
+//   functions,
+//   inngest,
+//   syncUserCreation,
+//   syncUserUpdation,
+//   syncUserDeletion,
+// } from "../../../config/ingest";
+// // import { helloWorld } from "../../../inngest/functions";
+
+// export const { GET, POST, PUT } = serve({
+//   client: inngest,
+//   functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
+// });
+
 import { serve } from "inngest/next";
 import {
-  functions,
   inngest,
   syncUserCreation,
   syncUserUpdation,
   syncUserDeletion,
-} from "../../../config/ingest";
-// import { helloWorld } from "../../../inngest/functions";
+} from "@/config/ingest.js"; // Add .js if ingest file is in JS
 
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
-});
+export const { GET, POST } = serve(inngest, [
+  syncUserCreation,
+  syncUserUpdation,
+  syncUserDeletion,
+]);
